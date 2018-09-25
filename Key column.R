7403f15953efd29229f87962616a5402b20f9f1d
madifelizardo <41930101+madifelizardo@users.noreply.github.com>
2018-09-26 09:16:16 +1000

    Key column

---
+++ Key column.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(lubridate)
+
+fertility <- read_csv("new fertility (Cut Down).csv")
+lifeexpectancy <- read_csv("new life expectancy (Cut Down).csv")
+education <- read_csv("new primary (Cut Down).csv")
+
+fertility1 <- fertility %>%
+  gather("Date", "Value", c(-'Country Name'))
+
+lifeexpectancy1 <- lifeexpectancy %>%
+  gather("Date", "Value", c(-'Country Name'))
+
+education1 <- education %>%
+  gather("Date", "Value", c(-'Country Name'))
+
+
+# Rename a column in R
+#colnames(data)[colnames(data)==“old_name”] <- “new_name”
+
+colnames(fertility1)[colnames(fertility1)=="Value"] <- "Fertility"
+colnames(lifeexpectancy1)[colnames(lifeexpectancy1)=="Value"] <- "Life Expectancy"
+colnames(education1)[colnames(education1)=="Value"] <- "Primary"
+
+LeftJoin <- fertility1 %>% 
+  left_join(lifeexpectancy1, by="Date") %>% 
+  left_join(education1, by="Date") %>% 
+  select(Date, 'Country Name', Fertility,'Life Expectancy', Primary)
+
+LeftJoin2 <-  LeftJoin %>% 
+  gather(key, value, c(-Date, -'Country Name'))
+
+View(LeftJoin2)
+
+#ggplot(LeftJoin2, mapping = aes(x = Date, y = value, group = key, colour = key)) + geom_line()
+
